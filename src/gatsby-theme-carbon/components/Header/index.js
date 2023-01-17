dbfd702b0638bb34e48a3ffaab0f489c72225214
Doug Slade <dugslade@us.ibm.com>
2023-01-17 02:11:45 -0500

    Update index.js

--- src/gatsby-theme-carbon/components/Header/index.js
+++ src/gatsby-theme-carbon/components/Header/index.js
@@ -3,7 +3,7 @@ import Header from 'gatsby-theme-carbon/src/components/Header';
 
 const CustomHeader = (props) => (
   <Header {...props}>
-    &nbsp;<span>Instana Partner 1-Day Workshop</span>
+    &nbsp;<span>Instana Partner 2-Day Advanced Workshop</span>
   </Header>
 );
 
